623794e23b5eb4d3a1c3bf8b19ff382980309426
joyamadea <amadeajoy@gmail.com>
2023-11-10 13:00:02 +0700

    Increasing max angular budget; Adding environment

--- src/app/shared/services/products.service.ts
+++ src/app/shared/services/products.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environment';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductsService {
-  url = "http://localhost:8080";
+  url = environment.BASE_URL;
 
   constructor(
     private http: HttpClient
